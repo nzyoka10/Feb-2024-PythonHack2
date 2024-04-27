d35de3e18fd9be3d727eabeac94ef4d18b5306d9
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-04-27 20:59:18 +0300

    server crashing again

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 DEBUG = True
 
 ALLOWED_HOSTS = [".replit.dev", ".replit.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '8000']
+
 CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
 
 # Application definition
